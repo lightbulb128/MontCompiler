ac70f16aa830b266394b7e0baacfd0f37bd47362
Liu Xuanqi <lxq18@mails.tsinghua.edu.cn>
2020-09-23 08:35:31 +0800

    Modify montLexer.cpp/h

--- src/montLexer.cpp
+++ src/montLexer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <fstream>
+#include <string.h>
 #include "montLexer.h"
 
 typedef LexAutomatonNode LNode;
@@ -15,9 +16,10 @@ LNode::LexAutomatonNode() {
 
 LNodePtr LNode::getNode(const char* path) {
     if (path[0]==0) return this;
-    if (children[path[0]] == nullptr) 
-        children[path[0]] = new LexAutomatonNode(); 
-    return children[path[0]]->getNode(path+1);
+    int c = path[0] - VALID_CHAR_OFFSET;
+    if (children[c] == nullptr) 
+        children[c] = new LexAutomatonNode(); 
+    return children[c]->getNode(path+1);
 }
 
 LNodePtr LNode::createNode(const char* path, const TokenKind token) {
@@ -34,6 +36,10 @@ LNode::~LexAutomatonNode() {
         }
 }
 
+LNode* LNode::transfer(char c){
+    return children[c];
+}
+
 LexAutomaton::LexAutomaton() {
     root = new LNode();
     currentPtr = root;
@@ -43,7 +49,60 @@ bool LexAutomaton::isAlphabet(char c) {
     return (c>='a' && c<='z') || (c>='A' && c<='Z') || (c=='_');
 }
 
+bool LexAutomaton::isLowercase(char c) { 
+    return (c>='a' && c<='z');
+}
+
 bool LexAutomaton::isNumber(char c) {
     return (c>='0' && c<='9');
 }
 
+bool LexAutomaton::isSymbol(char c){
+    int k = strlen(VALID_SYMBOLS);
+    for (int i=0;i<k;i++) if (c==VALID_SYMBOLS[i]) return true;
+    return false;
+}
+
+void LexAutomaton::openStream(const char* filename) {
+    stream.open(filename);
+}
+
+void LexAutomaton::reset(){
+    flagIdentifier = true;
+    flagSymbol = true;
+    flagKeyword = true;
+    currentPtr = root;
+    currentLength = 0;
+}
+
+LexAutomaton::TransferResult LexAutomaton::transfer(char c, char peek){
+    if (flagKeyword) {
+        if (isSymbol(c) || isSpace(c)) {
+            if (currentPtr->getTokenKind() != TK_UNDEFINED) {
+                currentToken = Token(currentPtr->getTokenKind());
+                return TR_PUTBACK;
+            } else {
+                flagKeyword = false;
+            }
+        } else if (isLowercase(c)) {
+            currentPtr = currentPtr->transfer(c);
+            if (currentPtr == nullptr) flagKeyword = false; 
+        } else {
+            flagKeyword = false;
+        }
+    } 
+    if (flagSymbol) {
+        switch (c) {
+            case '(': 
+                currentToken = Token(TK_LPAREN); return TR_FINISHED; break; 
+            case ')':
+                currentToken = Token(TK_RPAREN); return TR_FINISHED; break; 
+            case '{':
+                currentToken = Token(TK_LBRACE); return TR_FINISHED; break;
+            case '}':
+                currentToken = Token(TK_RBRACE); return TR_FINISHED; break;
+            case ';':
+                currentToken = Token(TK_SEMICOLON); return TR_FINISHED; break;  
+        }
+    }
+}
--- src/montLexer.h
+++ src/montLexer.h
@@ -1,4 +1,10 @@
-const int VALID_CHAR_RANGE = 128;
+#include <iostream>
+#include <fstream>
+
+const int   VALID_CHAR_RANGE = 26;
+const int   VALID_CHAR_OFFSET = 97;
+// const char* VALID_SYMBOLS    = "~!%^&*()-=+[]{}\\|;:'\"<>,./?";
+const char* VALID_SYMBOLS = "(){};><=";
 
 enum TokenKind {
     // identifier
@@ -9,6 +15,13 @@ enum TokenKind {
     TK_INT_VALUE, TK_CHAR_VALUE,
     // keywords
     TK_RETURN,
+    // symbols
+    TK_LPAREN, TK_RPAREN, TK_LBRACE, TK_RBRACE, TK_SEMICOLON,
+    TK_GREATER, TK_LESS, TK_GREATER_EQUAL, TK_LESS_EQUAL, 
+    TK_EQUAL,
+    TK_ASSIGN,
+    // eof
+    TK_EOF,
     // error
     TK_UNDEFINED
 };
@@ -17,6 +30,7 @@ struct Token{
     TokenKind tokenKind;
     int value;
     Token(TokenKind tokenKind, int value = 0);
+    Token(){tokenKind=TK_UNDEFINED; value=0;}
 };
 
 class LexAutomatonNode {
@@ -28,6 +42,8 @@ public:
     ~LexAutomatonNode();
     LexAutomatonNode* getNode(const char* path);
     LexAutomatonNode* createNode(const char* path, const TokenKind token);
+    LexAutomatonNode* transfer(char c);
+    TokenKind getTokenKind(){return tokenKind;}
 };
 
 /*
@@ -38,9 +54,27 @@ class LexAutomaton {
 private:
     LexAutomatonNode* root;
     LexAutomatonNode* currentPtr;
+    Token currentToken;
+    bool flagIdentifier;
+    bool flagSymbol;
+    bool flagKeyword;
+    int currentLength;
+    std::ifstream stream;
+    enum TransferResult{
+        TR_CONTINUE,
+        TR_FINISHED,
+        TR_PUTBACK,
+        TR_PEEKUSED
+    };
+    TransferResult transfer(char c, char peek);
+    void reset();
 public:
     LexAutomaton();
     static bool isAlphabet(char c); // underline is considered as an alphabet
     static bool isNumber(char c);
     static bool isSymbol(char c);
+    static bool isLowercase(char c);
+    static bool isSpace(char c){return c==' ';}
+    void openStream(const char* filename);
+    Token nextToken();
 };
